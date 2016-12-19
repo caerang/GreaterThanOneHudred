5a9c8c37e1252323a0245a5adaad2626ef84ec46
Sean Kim <singingmoi@gmail.com>
2016-12-19 22:46:01 +0900

    Initial Commit

---
+++ GreaterThanOneHudred/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GreaterThanOneHudred
+//
+//  Created by Han Moi Kim on 2016. 12. 19..
+//  Copyright © 2016년 dreamFactory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
